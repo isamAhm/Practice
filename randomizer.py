14e195c6e7b618b337f5b93acf1657a4840f6a6a
isamAhm <isam.ahmedh8@gmail.com>
2024-04-27 20:06:10 +0300

    commit one

---
+++ randomizer.py
@@ -0,0 +1,9 @@
+import random
+
+def generate_random_number():
+    # Generates a random integer between 1 and 100
+    return random.randint(1, 100)
+
+# Example usage
+random_number = generate_random_number()
+print("Random number:", random_number)
