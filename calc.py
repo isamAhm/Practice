2cd6539a0fe9e790003576569232acb99c4885db
Isam Ahmed <167436033+isamAhm@users.noreply.github.com>
2024-04-28 14:35:15 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
-first_num = int(input("First Number: "))
-second_num = int(input("Second Number: "))
+first_num = int(input("Please enter your First Number: "))
+second_num = int(input("Please endter your Second Number: "))
 sum = first_num + second_num
 
 print(sum)
